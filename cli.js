713bab817436fbb6c792f5336f7f643a8fdb370a
Edemone <edem.agbenyo@gmail.com>
2019-08-30 08:55:58 +0000

    Initial commit

---
+++ cli.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const inquirer = require('inquirer');
+const opn = require('open');
+const edem = require('./src/edem');
+
+program.version('1.0.0')
+    .description('Edem on your command-line')
+    .parse(process.argv);
+
+console.log(edem.bio);
+inquirer.prompt({
+    name: 'link',
+    type: 'list',
+    message: edem.prompt,
+    choices: edem.links.concat({
+        'name': `...Or shoot me an email (${edem.email})`,
+        'value': 'mailto:' + edem.email
+    })
+}).then(answers => {
+    console.log(`Opening ${answers.link}`);
+    opn(answers.link);
+});
---
+++ src/edem.js
@@ -0,0 +1,21 @@
+const edem = {
+    bio: "Hi, I'm Edem Agbenyo. I'm a full stack software engineer based in Accra, Ghana.",
+    prompt: 'Want to know more about me? Check me out on these sites:',
+    links: [
+        {
+            name: 'GitHub',
+            value: 'https://github.com/edemagbenyo',
+        },
+        {
+            name: 'dev.to',
+            value: 'https://dev.to/edemagbenyo',
+        },
+        {
+            name: 'Twitter',
+            value: 'https://twitter.com/edemagbenyo',
+        }
+    ],
+    email: 'hello@edemagbenyo.com'
+};
+
+module.exports = edem;
